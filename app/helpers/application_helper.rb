30cb02eccc614d3193ab2f90c266d365e2c43dad
myzk-a <akitoshi19910309@gmail.com>
2021-10-26 06:14:58 +0000

    add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "ASMobi : 工数管理システム"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
