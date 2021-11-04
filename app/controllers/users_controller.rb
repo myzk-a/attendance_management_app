b9262658000f5dbccfc4e19b5f8a4fe1288f7998
myzk-a <akitoshi19910309@gmail.com>
2021-11-04 06:16:11 +0000

    Add user edit, update, index, and destory actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def show
     @user = User.find(params[:id])
   end
@@ -17,9 +25,56 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+  
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "設定を変更しました"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    user = User.find_by(id: params[:id])
+    if !user.admin?
+      User.find_by(id: params[:id]).destroy
+      flash[:success] = "ユーザーを削除しました"
+    end
+    redirect_to users_url
+  end
+  
+  
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+    
+    # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
+    
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
   
   validates :name,  presence: true, length: {maximum: 20}
   
-  VALID_EMAIL_REGEX = /\A[a-z]+_+[a-z]+@as-mobi\.com+\z/
+  VALID_EMAIL_REGEX = /\A[a-z\d]+_+[a-z]+@as-mobi\.com+\z/
   validates :email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}, uniqueness: true
   
   has_secure_password
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
   
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
