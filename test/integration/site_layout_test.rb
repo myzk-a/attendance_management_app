d9a6196a6d71ec48263cb27fbab9ae9d3be94346
myzk-a <akitoshi19910309@gmail.com>
2021-11-08 10:19:49 +0000

    add admin user page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -26,9 +26,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", root_path,      count: 0
-    assert_select "a[href=?]", users_path,     count: 1
-    assert_select "a[href=?]", signup_path,    count: 1
-    assert_select "a[href=?]", edit_user_path(@admin_user), count: 1
+    assert_select "a[href=?]", users_path,     count: 2
+    assert_select "a[href=?]", signup_path,    count: 2
+    assert_select "a[href=?]", edit_user_path(@admin_user), count: 2
     assert_select "a[href=?]", logout_path,    count: 1
     assert_select "a[href=?]", login_path,     count: 0
     assert_select "title", full_title("#{@admin_user.name}")
