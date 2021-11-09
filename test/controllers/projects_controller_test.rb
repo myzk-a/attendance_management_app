00824fdd7ac3cf1290c99701469d6a3ae44a5aeb
myzk-a <akitoshi19910309@gmail.com>
2021-11-09 07:19:22 +0000

    add project delete test

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -53,4 +53,19 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
+  test "should redirect destroy when logged in nonadmin user" do
+    log_in_as(@nonadmin_user)
+    assert_no_difference 'Project.count' do
+      delete project_path(@project)
+    end
+    assert_redirected_to root_url
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Project.count' do
+      delete project_path(@project)
+    end
+    assert_redirected_to root_url
+  end
+
 end
