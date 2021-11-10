c194ae17f0fc0c2e575b1ffa9f12a00c4f336e9f
myzk-a <akitoshi19910309@gmail.com>
2021-11-10 09:46:00 +0000

    fix projects index layout

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
+  default_scope -> { order(:code) }
   validates :name, presence: true, length: {maximum: 30}, uniqueness: true
   validates :code, presence: true, length: {maximum: 30}, uniqueness: true
 end
