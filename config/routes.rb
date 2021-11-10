6805b30e483d6586b9345322f9d4a6a1cffbfd5c
myzk-a <akitoshi19910309@gmail.com>
2021-11-10 06:31:54 +0000

    fix project update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get    '/signup',          to: 'users#new'
-  post   '/signup',          to: 'users#create'
-  get    '/login',           to: 'sessions#new'
-  post   '/login',           to: 'sessions#create'
-  delete '/logout',          to: 'sessions#destroy'
-  get    '/projects/signup', to: 'projects#new'
-  post   '/projects/signup', to: 'projects#create'
+  get    '/signup',              to: 'users#new'
+  post   '/signup',              to: 'users#create'
+  get    '/login',               to: 'sessions#new'
+  post   '/login',               to: 'sessions#create'
+  delete '/logout',              to: 'sessions#destroy'
+  get    '/projects/signup',     to: 'projects#new'
+  post   '/projects/signup',     to: 'projects#create'
+  patch  '/projects/:id/edit',   to: 'projects#update'
   resources :users
-  resources :projects
+  resources :projects, except: [:show]
 end
