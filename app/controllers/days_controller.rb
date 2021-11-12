00a377c896bc3fcdc1bc8e409c974cb517edbf1d
myzk-a <akitoshi19910309@gmail.com>
2021-11-12 08:27:29 +0000

    add works table

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,16 +1,18 @@
 class DaysController < ApplicationController
-  before_action :get_works, only: [:show]
+  before_action :get_works, only: [:new, :show]
+
+  def new
+  end
 
   def show
-    
   end
 
   private
 
     def get_works
-      date = DateTime.parse(params[:id])
+      date = DateTime.parse(params[:day])
       range = date.beginning_of_day..date.end_of_day
-      @works = Work.where(start_time: range)
+      @works = Work.where(user_id:params[:user_id] ,start_time: range)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get    '/signup',            to: 'users#new'
-  post   '/signup',            to: 'users#create'
-  get    '/login',             to: 'sessions#new'
-  post   '/login',             to: 'sessions#create'
-  delete '/logout',            to: 'sessions#destroy'
-  patch  '/users/:id/edit',    to: 'users#update'
-  get    '/projects/signup',   to: 'projects#new'
-  post   '/projects/signup',   to: 'projects#create'
-  patch  '/projects/:id/edit', to: 'projects#update'
+  get    '/signup',                 to: 'users#new'
+  post   '/signup',                 to: 'users#create'
+  get    '/login',                  to: 'sessions#new'
+  post   '/login',                  to: 'sessions#create'
+  delete '/logout',                 to: 'sessions#destroy'
+  patch  '/users/:id/edit',         to: 'users#update'
+  get    '/projects/signup',        to: 'projects#new'
+  post   '/projects/signup',        to: 'projects#create'
+  patch  '/projects/:id/edit',      to: 'projects#update'
+  get    '/days/:user_id/:day/new', to: 'days#new'
+  get    '/days/:user_id/:day',     to: 'days#show'
   resources :users
   resources :projects, except: [:show]
-  resources :days
 end
