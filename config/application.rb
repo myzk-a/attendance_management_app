fe26be977796730ba2df67bc3771f62e2d8c072e
myzk-a <akitoshi19910309@gmail.com>
2021-11-14 00:27:45 +0000

    change time zone

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module AttendanceManagemetApp
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.time_zone = 'Tokyo'
   end
 end
