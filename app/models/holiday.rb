fb4b2ce49af062cdb92e74f1a8487421af21e5a8
myzk-a <akitoshi19910309@gmail.com>
2021-11-18 11:58:19 +0900

    add order

--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,4 +1,5 @@
 class Holiday < ApplicationRecord
+  default_scope -> { order(:date) }
   validates :name, presence: true, length: {maximum: 20}
   validates :date, presence: true, uniqueness: true
 end
