1aaf96def38653bdd3b4c5a15ee73f40bf1d55dc
myzk-a <akitoshi19910309@gmail.com>
2021-11-18 12:24:51 +0900

    add holiday delete

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -1,7 +1,7 @@
 class HolidaysController < ApplicationController
   include SessionsHelper
 
-  before_action :admin_user, only: [:new, :create, :edit]
+  before_action :admin_user, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @holidays = Holiday.all
@@ -15,7 +15,7 @@ class HolidaysController < ApplicationController
     @holiday = Holiday.new(holiday_params)
     if @holiday.save
       flash[:success] = "休日を登録しました。"
-      redirect_to holidays_path
+      redirect_to holidays_url
     else
       render 'new'
     end
@@ -29,13 +29,19 @@ class HolidaysController < ApplicationController
     @holiday = Holiday.find_by(id: params[:id])
     if @holiday.update_attributes(holiday_params)
       flash[:success] = "登録内容を変更しました。"
-      redirect_to holidays_path
+      redirect_to holidays_url
     else
       render 'edit'
     end
   end
 
   def destroy
+    holiday = Holiday.find_by(id: params[:id])
+    unless holiday.nil?
+      holiday.destroy
+      flash[:success] = "登録内容を削除しました。"
+    end
+    redirect_to holidays_url
   end
 
   private
