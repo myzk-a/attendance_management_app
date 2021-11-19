93aa04dca9bad27556e2d146384b917271dac2da
myzk-a <akitoshi19910309@gmail.com>
2021-11-19 11:33:00 +0900

    fix calender style

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+  def date_is_holiday?(date)
+    holiday = Holiday.find_by(date: date)
+    if date.saturday? || date.sunday? || holiday.present?
+      if holiday.present?
+        return holiday.name
+      else
+        return "weekend"
+      end
+    else
+      return false
+    end
+  end
 end
