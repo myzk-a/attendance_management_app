f837ca381271b3df46b1870726674e01c35e9db2
myzk-a <akitoshi19910309@gmail.com>
2021-11-21 16:41:02 +0900

    add holidays signup test

---
+++ test/integration/holidays_signup_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class HolidaysSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    @admin_user = users(:SYATYO)
+  end
+
+  test "invalid signup information" do
+    log_in_as(@admin_user)
+    get holidays_signup_path
+    assert_no_difference 'Holiday.count' do
+      post holidays_signup_path, params: { holiday: { name: "", date: "" } }
+    end
+    assert_template 'holidays/new'
+  end
+
+  test "valid sign up information" do
+    log_in_as(@admin_user)
+    get holidays_signup_path
+    assert_difference 'Holiday.count' do
+      post holidays_signup_path, params: { holiday: { name: "休日", date: DateTime.now.to_date } }
+    end
+    follow_redirect!
+    assert_template 'holidays/index'
+    assert_not flash.empty?
+  end
+
+end
