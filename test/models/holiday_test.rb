105aa9952f6ed7d6f92b5cbf67cba1729f768e46
myzk-a <akitoshi19910309@gmail.com>
2021-11-21 16:19:18 +0900

    add holiday model test

--- test/models/holiday_test.rb
+++ test/models/holiday_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class HolidayTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @holiday = holidays(:holiday)
+  end
+
+  test "should be valid" do
+    assert @holiday.valid?
+  end
+
+  test "name should be present" do
+    @holiday.name = " "
+    assert_not @holiday.valid?
+  end
+
+  test "date should be preset" do
+    @holiday.date = ""
+    assert_not @holiday.valid?
+  end
+
+  test "name should not be too long" do
+    @holiday.name = "a"*21
+    assert_not @holiday.valid?
+  end
+
+  test "date should be unique" do
+    duplicate_holiday = @holiday.dup
+    @holiday.save
+    duplicate_holiday.name = "test"
+    assert_not duplicate_holiday.valid?
+  end
+
 end
