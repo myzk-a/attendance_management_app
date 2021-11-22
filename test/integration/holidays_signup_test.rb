dc7caaa06b49ed4e5e9eded329b99dd9f6c8bbab
myzk-a <akitoshi19910309@gmail.com>
2021-11-22 10:59:51 +0900

    add csv upload test

--- test/integration/holidays_signup_test.rb
+++ test/integration/holidays_signup_test.rb
@@ -25,4 +25,55 @@ class HolidaysSignupTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
   end
 
+  test "valid sign up information via valid csv" do
+    log_in_as(@admin_user)
+    get holidays_path
+    assert_template 'holidays/index'
+    get holidays_path, params: { search: { year: "2020" } }
+    assert_select "table", count: 0
+    get holidays_signup_path
+    assert_template 'holidays/new'
+    holidays_csv = fixture_file_upload("/files/valid_holidays.csv", "text/csv")
+    post import_holidays_path, params: { file: holidays_csv }
+    follow_redirect!
+    assert_template 'holidays/index'
+    assert_not flash.empty?
+    get holidays_path, params: { search: { year: "2020" } }
+    assert_select "table", count: 1
+  end
+
+  test "invalid sign up information via invalid csv" do
+    log_in_as(@admin_user)
+    get holidays_signup_path
+    assert_template 'holidays/new'
+    holidays_csv = fixture_file_upload("/files/invalid_holidays.csv", "text/csv")
+    post import_holidays_path, params: { file: holidays_csv }
+    follow_redirect!
+    assert_template 'holidays/index'
+    get holidays_path, params: { search: { year: "2020" } }
+    assert_select "table", count: 0
+  end
+
+  test "invalid sign up information via invalid extension file" do
+    log_in_as(@admin_user)
+    get holidays_signup_path
+    assert_template 'holidays/new'
+    holidays_txt = fixture_file_upload("/files/valid_holidays.txt", "text/plain")
+    post import_holidays_path, params: { file: holidays_txt }
+    follow_redirect!
+    assert_not flash.empty?
+    assert_template 'holidays/new'
+  end
+
+  test "invalid sign up information when not selected csv file" do
+    log_in_as(@admin_user)
+    get holidays_signup_path
+    assert_template 'holidays/new'
+    holidays_csv = nil
+    post import_holidays_path, params: { file: holidays_csv }
+    follow_redirect!
+    assert_not flash.empty?
+    assert_template 'holidays/new'
+  end
+
 end
